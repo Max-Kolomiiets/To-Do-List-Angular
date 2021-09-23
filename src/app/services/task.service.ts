53490183fee1ac489aff134fb68ff148f77beb52
Max <kmaks1364@gmail.com>
2021-09-23 15:31:56 +0300

    added json-server and db.json file

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
@@ -7,10 +8,11 @@ import { TASKS } from 'src/app/mock-tasks';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
